7cd1bb5f4caf84bf5fab2e38d85b73864bb8f81d
Hayk Ghazaryan <ghazaryan.h@gmail.com>
2024-10-31 22:34:44 +0400

    Minor improvements

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { homeLoader } from "./loaders/homeLoader.ts";
 import { previewLoader } from "./loaders/previewLoader.ts";
 
--- src/services/pexels.api.ts
+++ src/services/pexels.api.ts
@@ -2,4 +2,8 @@ import { createClient } from "pexels";
 
 export const HIGHEST_PER_PAGE_SIZE = 80;
 
+if (!import.meta.env.VITE_PEXELS_API_KEY) {
+  alert("Environment variable is missing: VITE_PEXELS_API_KEY");
+}
+
 export const pexelsClient = createClient(import.meta.env.VITE_PEXELS_API_KEY);
